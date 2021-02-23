6991d7b971cbf0b5e363f3a2e78e41b2d066ead9
Ivan Reyes <ivinreyes1@gmail.com>
2021-02-23 03:10:09 -0600

    first commit exercise 2

---
+++ examen_ejercicio2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace examen_ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese el primer numero");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese el tercer numero");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("ingrese el cuarto numero");
+            double num4 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("La suma de los dos primeros numeros es:");
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine("El producto de los dos ultimos numeros es:");
+            Console.WriteLine(num3*num4);
+            Console.ReadKey();
+        }
+    }
+}
