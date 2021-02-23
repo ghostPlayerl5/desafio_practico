2e0658ee9a73bbfc03854943ff667976645f91bf
Ivan Reyes <ivinreyes1@gmail.com>
2021-02-23 03:13:14 -0600

    first commit exercise 3

---
+++ examen_ejercicio3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace examen_ejercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese la primera nota");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("ingrese la segunda nota");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("ingrese la tercera nota ");
+            double num3 = Convert.ToDouble(Console.ReadLine());
+
+            double prom = (num1 + num2 + num3) / 3;
+
+            if (prom >= 7)
+            {
+                Console.WriteLine("Promocionado");
+            }
+            else if (prom >= 4)
+            {
+                Console.WriteLine("regular");
+            }
+            else if (prom < 4)
+            {
+                Console.WriteLine("reprobado");
+
+            }
+        }
+    }
+}
